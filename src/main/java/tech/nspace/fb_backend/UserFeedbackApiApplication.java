012a0985e0e552f9c874cbbc1c08deca016c9ea5
Kevinnguefack <nguefackkevin66@gmail.com>
2025-10-19 12:23:58 +0200

    first commit

---
+++ src/main/java/tech/nspace/fb_backend/UserFeedbackApiApplication.java
@@ -0,0 +1,13 @@
+package tech.nspace.fb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserFeedbackApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserFeedbackApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/nspace/fb_backend/UserFeedbackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nspace.fb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserFeedbackApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
